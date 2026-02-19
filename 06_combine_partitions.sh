94717debc68f3571b2ad66ab67e91c0ff426a4c9
Jason Stajich <jasonstajich.phd@gmail.com>
2026-02-18 22:02:56 -0800

    Enhance comments in 06_combine_partitions.sh
    Added comments to clarify the script's functionality.

--- 06_combine_partitions.sh
+++ 06_combine_partitions.sh
@@ -3,11 +3,12 @@
 # use 8 threads, more if you have a more beefy computer
 CPU=8
 conda activate phylogenetics
-
+# will create a partition file and combined multi-fasta alignment then run modeltest to determine per-alignment model test
 pushd aligned_renamed
 ls *.mfa > matrix.txt
 phykit cc -a matrix.txt -p combined_aligned
 perl -i -p -e 's/^AUTO/DNA/; s/\.mfa//' combined_aligned.partition
 modeltest-ng -i combined_aligned.fa -q combined_aligned.partition -T raxml -p $CPU
+# just using this, could use *.aicc or *.bic if you prefer
 cp *.aic *.fa ..
 popd
